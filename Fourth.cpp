5bd0b4fab36a3e0760714949440d5f0eb30ac579
Neeraj Kumar <neerajkumar42p@gmail.com>
2024-06-28 15:12:36 +0530

    commit

---
+++ Fourth.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b;
+    int c,d;
+    cout<<"Input 4 integers a,b,c and d"<<endl;
+    cin>>a;
+    cin>>b;
+    cin>>c;
+    cin>>d;
+    cout<<(a>b&&c>d);
+}
